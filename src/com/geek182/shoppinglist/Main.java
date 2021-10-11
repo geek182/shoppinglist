f75fe13521c995cb22911db0e6e53b35c77a3895
Leandro Azevedo <leandro.s.azevedo@gmail.com>
2021-10-11 17:52:00 +0200

    gitignore and delete unwanted files

--- src/com/geek182/shoppinglist/Main.java
+++ src/com/geek182/shoppinglist/Main.java
@@ -12,12 +12,24 @@ public class Main {
         //mylist.AddItem("Milk", 2);
         //mylist.GetItens();
 
-        Item mylist = new Item();
+/*        Item mylist = new Item();
         mylist.CreateList("Leandro", "Weekly", "List for week");
         mylist.GetList("Weekly");
         mylist.AddItem("Pao",2);
         mylist.AddItem("Milk",1);
         mylist.GetItens();
+*/
+        List mylist = new List();
+        mylist.CreateList("Leandro","Weekly","List for week");
+        mylist.CreateItens("Pao",2);
+        mylist.CreateItens("Milk",2);
+        mylist.CreateItens("Kaas",1);
+        mylist.GetItens();
+
+        List mylist2 = new List();
+        mylist2.CreateList("Leo","test", "test2");
+        mylist2.CreateItens("soup",2);
+        mylist2.GetItens();
 
 
 
